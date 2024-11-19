9412a2a1500b02acd9f1f22c3cf55faab6bb3b98
Davidenko01 <davor.kissner@est.fi.uncoma.edu.ar>
2024-11-19 17:54:48 -0300

    get de cantidad terminado

--- controllers/tablasController.js
+++ controllers/tablasController.js
@@ -3,14 +3,42 @@ const { getLigaDataById } = require('../models/tablasModel');
 const getTableData = (req, res) => {
   const liga = req.query.liga;
   const ligaData = getLigaDataById(liga);
+  if (!liga) {
+    return res.status(400).json({ error: 'Parametro liga no especificado' });
+  }
 
   if (!ligaData) {
     return res.status(400).json({ error: 'Liga no válida' });
   }
+  
   res.json(ligaData);
 };
 
-module.exports = { getTableData };
+const getCantidad = (req, res) => {
+  const cantidad = req.query.cantidad;
+  const from = req.query.from;
+  const liga = req.params.liga;
+  const ligaData = getLigaDataById(liga);
+
+  if (!ligaData) {
+    return res.status(400).json({ error: 'Liga no válida' });
+  }
+
+  if (!cantidad && !from) {
+    return res.status(400).json({ error: 'Parametro rango no especificado' });
+  }
+  if (from < 0 && cantidad <= from && (cantidad + from) >= (ligaData.positions.length)) {
+    return res.status(400).json({ error: 'Parametros no válidos' });
+  } 
+
+  const positions = ligaData.positions;
+  const paginatedPositions = positions.slice(from, from + cantidad);
+
+  return res.json(paginatedPositions);
+
+};
+
+module.exports = { getTableData , getCantidad};
 
 
 
